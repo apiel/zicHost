d917c84f8c2bc63de6e4c4fc5c70d6afc17bb28b
Alexandre Piel <alexandre.piel@zooplus.com>
2023-08-25 22:59:18 +0200

    font

--- interface/ui/def.h
+++ interface/ui/def.h
@@ -5,15 +5,27 @@
 #include <SDL2/SDL_ttf.h>
 
 #ifndef APP_FONT
-#define APP_FONT "/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf"
+// #define APP_FONT "/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf"
+// #define APP_FONT "/usr/share/fonts/truetype/freefont/FreeSans.ttf"
+// #define APP_FONT "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
+// #define APP_FONT "/usr/share/fonts/truetype/roboto/unhinted/RobotoCondensed-Regular.ttf"
+#define APP_FONT "/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Regular.ttf"
 #endif
 
 #ifndef APP_FONT_BOLD
-#define APP_FONT_BOLD "/usr/share/fonts/truetype/liberation2/LiberationSans-Bold.ttf"
+// #define APP_FONT_BOLD "/usr/share/fonts/truetype/liberation2/LiberationSans-Bold.ttf"
+// #define APP_FONT_BOLD "/usr/share/fonts/truetype/freefont/FreeSansBold.ttf"
+// #define APP_FONT_BOLD "/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf"
+// #define APP_FONT_BOLD "/usr/share/fonts/truetype/roboto/unhinted/RobotoCondensed-Bold.ttf"
+#define APP_FONT_BOLD "/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Bold.ttf"
 #endif
 
 #ifndef APP_FONT_ITALIC
-#define APP_FONT_ITALIC "/usr/share/fonts/truetype/liberation2/LiberationSans-Italic.ttf"
+// #define APP_FONT_ITALIC "/usr/share/fonts/truetype/liberation2/LiberationSans-Italic.ttf"
+// #define APP_FONT_ITALIC "/usr/share/fonts/truetype/freefont/FreeSansOblique.ttf"
+// #define APP_FONT_ITALIC "/usr/share/fonts/truetype/freefont/FreeMonoOblique.ttf"
+// #define APP_FONT_ITALIC "/usr/share/fonts/truetype/roboto/unhinted/RobotoCondensed-Italic.ttf"
+#define APP_FONT_ITALIC "/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Italic.ttf"
 #endif
 
 #ifndef APP_DEFAULT_FONT_SIZE
