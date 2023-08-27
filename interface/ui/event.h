62deeee86b65f30cc63c212440943efdaf5739b7
Alex tablet <alexandre.piel@gmail.com>
2023-08-27 22:54:21 +0200

    motion

--- interface/ui/event.h
+++ interface/ui/event.h
@@ -1,10 +1,29 @@
 #ifndef _UI_EVENT_H_
 #define _UI_EVENT_H_
 
+#include "../interfacePlugin.h"
 #include "def.h"
 #include "state.h"
-#include "../interfacePlugin.h"
 #include "viewMain.h"
+
+int lastLog = 0;
+void handleMotion(int x, int y, int id)
+{
+    if (SDL_GetTicks() - lastLog > 100) {
+        lastLog = SDL_GetTicks();
+        SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Motion %d, %d (id: %d)", x, y, id);
+    }
+}
+
+void handleMotionUp(int x, int y, int id)
+{
+    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "MotionUp %d, %d (id: %d)", x, y, id);
+}
+
+void handleMotionDown(int x, int y, int id)
+{
+    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "MotionDown %d, %d (id: %d)", x, y, id);
+}
 
 bool handleEvent()
 {
@@ -15,6 +34,30 @@ bool handleEvent()
         case SDL_QUIT:
             SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Quit");
             return false;
+
+        case SDL_MOUSEMOTION:
+            handleMotion(event.motion.x, event.motion.y, event.motion.which);
+            return true;
+
+        case SDL_MOUSEBUTTONDOWN:
+            handleMotionDown(event.motion.x, event.motion.y, event.motion.which);
+            return true;
+
+        case SDL_MOUSEBUTTONUP:
+            handleMotionUp(event.motion.x, event.motion.y, event.motion.which);
+            return true;
+
+        case SDL_FINGERMOTION:
+            handleMotion(event.tfinger.x, event.tfinger.y, event.tfinger.fingerId);
+            return true;
+
+        case SDL_FINGERDOWN:
+            handleMotionDown(event.tfinger.x, event.tfinger.y, event.tfinger.fingerId);
+            return true;
+
+        case SDL_FINGERUP:
+            handleMotionUp(event.tfinger.x, event.tfinger.y, event.tfinger.fingerId);
+            return true;
         }
     }
     return true;
