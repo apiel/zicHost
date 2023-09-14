53465cf22ae4661ac1dc00cbef9bf810572e85c2
Alexandre Piel <alexandre.piel@zooplus.com>
2023-09-14 22:54:57 +0200

    pulse

--- audioApi_pulse.h
+++ audioApi_pulse.h
@@ -8,58 +8,9 @@
 #include "audioHandler.h"
 #include "def.h"
 
-static void pa_set_sink_info(pa_context* c, const pa_sink_info* i,
-    int eol, void* userdata)
-{
-    if (eol) {
-        // pa_mainloop_api* mlApi = (pa_mainloop_api*)userdata;
-        // mlApi->quit(mlApi, 0);
-        return;
-    }
-
-    // printf("name: %s (outputChannels %d preferredSampleRate %d)\n", pa_proplist_gets(i->proplist, "device.description"), i->sample_spec.channels, i->sample_spec.rate);
-    APP_PRINT("- %s [AUDIO_OUTPUT=%s]\n", pa_proplist_gets(i->proplist, "device.description"), i->name);
-}
-
-static void pa_set_source_info(pa_context* c, const pa_source_info* i,
-    int eol, void* userdata)
-{
-    if (eol) {
-        pa_mainloop_api* mlApi = (pa_mainloop_api*)userdata;
-        mlApi->quit(mlApi, 0);
-        return;
-    }
-
-    // printf("name: %s (inputChannels %d preferredSampleRate %d)\n", pa_proplist_gets(i->proplist, "device.description"), i->sample_spec.channels, i->sample_spec.rate);
-    APP_PRINT("- %s [AUDIO_INPUT=%s]\n", pa_proplist_gets(i->proplist, "device.description"), i->name);
-}
-
-static void pa_context_state_callback(pa_context* context, void* userdata)
-{
-    pa_mainloop_api* mlApi = (pa_mainloop_api*)userdata;
-
-    switch (pa_context_get_state(context)) {
-    case PA_CONTEXT_CONNECTING:
-    case PA_CONTEXT_AUTHORIZING:
-    case PA_CONTEXT_SETTING_NAME:
-        break;
-
-    case PA_CONTEXT_READY:
-        debug("PA_CONTEXT_READY\n");
-        pa_context_get_sink_info_list(context, pa_set_sink_info, userdata); // output info ... needs to be before input
-        pa_context_get_source_info_list(context, pa_set_source_info, userdata); // input info
-        break;
-
-    case PA_CONTEXT_TERMINATED:
-        mlApi->quit(mlApi, 0);
-        break;
-
-    case PA_CONTEXT_FAILED:
-    default:
-        debug("PA_CONTEXT_FAILED\n");
-        mlApi->quit(mlApi, 0);
-    }
-}
+static void pa_context_state_callback(pa_context* context, void* userdata);
+static void pa_set_sink_info(pa_context* c, const pa_sink_info* i, int eol, void* userdata);
+static void pa_set_source_info(pa_context* c, const pa_source_info* i, int eol, void* userdata);
 
 class AudioApiPulse : public AudioApi {
 protected:
@@ -89,14 +40,14 @@ protected:
             return freeListDevice();
         }
 
-        pa_mainloop_api* paMainLoopApi = pa_mainloop_get_api(ml);
+        paMainLoopApi = pa_mainloop_get_api(ml);
 
         if (!(context = pa_context_new_with_proplist(paMainLoopApi, NULL, NULL))) {
             debug("pa_context_new() failed.\n");
             return freeListDevice();
         }
 
-        pa_context_set_state_callback(context, pa_context_state_callback, paMainLoopApi);
+        pa_context_set_state_callback(context, pa_context_state_callback, this);
 
         if (pa_context_connect(context, server, PA_CONTEXT_NOFLAGS, NULL) < 0) {
             debug("pa_context_connect() failed: %s\n", pa_strerror(pa_context_errno(context)));
@@ -111,6 +62,10 @@ protected:
     }
 
 public:
+    const char* foundInputName = NULL;
+    const char* foundOutputName = NULL;
+    pa_mainloop_api* paMainLoopApi;
+
     static AudioApiPulse& get()
     {
         if (!instance) {
@@ -129,8 +84,8 @@ public:
             .channels = APP_CHANNELS,
         };
 
-        pa_simple* deviceOut = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, audioOutputName, "zicGranular", &streamFormat, NULL, NULL, NULL);
-        pa_simple* deviceIn = pa_simple_new(NULL, NULL, PA_STREAM_RECORD, audioInputName, "zicGranular", &streamFormat, NULL, NULL, NULL);
+        pa_simple* deviceOut = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, foundOutputName == NULL ? audioOutputName : foundOutputName, "zicGranular", &streamFormat, NULL, NULL, NULL);
+        pa_simple* deviceIn = pa_simple_new(NULL, NULL, PA_STREAM_RECORD, foundInputName == NULL ? audioInputName : foundInputName, "zicGranular", &streamFormat, NULL, NULL, NULL);
 
         uint8_t bufferMultiplier = sizeof(float) / sizeof(uint8_t);
 
@@ -156,5 +111,68 @@ public:
 };
 
 AudioApiPulse* AudioApiPulse::instance = NULL;
+
+static void pa_set_sink_info(pa_context* c, const pa_sink_info* i, int eol, void* userdata)
+{
+    if (eol) {
+        // AudioApiPulse* api = (AudioApiPulse*)userdata;
+        // api->paMainLoopApi->quit(api->paMainLoopApi, 0);
+        return;
+    }
+
+    const char* description = pa_proplist_gets(i->proplist, "device.description");
+    // printf("name: %s (outputChannels %d preferredSampleRate %d)\n", pa_proplist_gets(i->proplist, "device.description"), i->sample_spec.channels, i->sample_spec.rate);
+    APP_PRINT("- %s [AUDIO_OUTPUT=%s]\n", description, i->name);
+
+    if (strcmp(description, audioOutputName) == 0) {
+        AudioApiPulse* api = (AudioApiPulse*)userdata;
+        api->foundOutputName = i->name;
+    }
+}
+
+static void pa_set_source_info(pa_context* c, const pa_source_info* i, int eol, void* userdata)
+{
+    if (eol) {
+        AudioApiPulse* api = (AudioApiPulse*)userdata;
+        api->paMainLoopApi->quit(api->paMainLoopApi, 0);
+        return;
+    }
+
+    const char* description = pa_proplist_gets(i->proplist, "device.description");
+    // printf("name: %s (inputChannels %d preferredSampleRate %d)\n", pa_proplist_gets(i->proplist, "device.description"), i->sample_spec.channels, i->sample_spec.rate);
+    APP_PRINT("- %s [AUDIO_INPUT=%s]\n", description, i->name);
+
+    if (strcmp(description, audioInputName) == 0) {
+        AudioApiPulse* api = (AudioApiPulse*)userdata;
+        api->foundInputName = i->name;
+    }
+}
+
+static void pa_context_state_callback(pa_context* context, void* userdata)
+{
+    AudioApiPulse* api = (AudioApiPulse*)userdata;
+
+    switch (pa_context_get_state(context)) {
+    case PA_CONTEXT_CONNECTING:
+    case PA_CONTEXT_AUTHORIZING:
+    case PA_CONTEXT_SETTING_NAME:
+        break;
+
+    case PA_CONTEXT_READY:
+        debug("PA_CONTEXT_READY\n");
+        pa_context_get_sink_info_list(context, pa_set_sink_info, userdata); // output info ... needs to be before input
+        pa_context_get_source_info_list(context, pa_set_source_info, userdata); // input info
+        break;
+
+    case PA_CONTEXT_TERMINATED:
+        api->paMainLoopApi->quit(api->paMainLoopApi, 0);
+        break;
+
+    case PA_CONTEXT_FAILED:
+    default:
+        debug("PA_CONTEXT_FAILED\n");
+        api->paMainLoopApi->quit(api->paMainLoopApi, 0);
+    }
+}
 
 #endif
